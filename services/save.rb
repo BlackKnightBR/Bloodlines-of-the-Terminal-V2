86b3bc13e2012ff617104a26b94b771871d4535b
RODRIGO MONTEBELLO SABOYA BRITO <blackknight@blackknightstudio.com.br>
2025-07-16 10:18:26 -0300

    Refactor Save system
    💾 Refactor Save system using Marshal for binary persistence of player and inventory data

--- services/save.rb
+++ services/save.rb
@@ -1,75 +1,38 @@
-require 'yaml'
-
 class Save
   def initialize(player:, inventory:)
     @player = player
     @inventory = inventory
   end
 
-  def save(filename = "save.yaml")
-    data = {
-      player: {
-        name: @player.name,
-        hp: @player.hp,
-        attack: @player.attack,
-        defense: @player.defense,
-        level: @player.level,
-        xp: @player.xp,
-        type: @player.type
-      },
-      inventory: @inventory.items.map do |item, (qty, type)|
-        {
-          name: item.name,
-          type: item.class.to_s,
-          quantity: qty,
-          owner: type
-        }
-      end
-    }
-
-    File.write(filename, data.to_yaml)
-    puts "🔒 Progress saved to #{filename}."
+  def save_object()
+    filename1 = "player.save"
+    filename2 = "inventory.save"
+    File.open(filename1, 'wb') do |file|
+      Marshal.dump(@player,file)
+    end
+    File.open(filename2, 'wb') do |file|
+      Marshal.dump(@inventory,file)
+    end
+    puts "🧪 Game state saved to #{filename1} and #{filename2}"
   end
 
-  def self.load(filename = "save.yaml")
+  def load_player(filename = "player.save")
     return nil unless File.exist?(filename)
-    data = YAML.load_file(filename)
 
-    # reconstruir jogador
-    player_data = data[:player]
-    player = case player_data[:type]
-             when 'warrior'
-               Warrior.new(
-                 name: player_data[:name],
-                 hp: player_data[:hp],
-                 attack: player_data[:attack],
-                 defense: player_data[:defense],
-                 level: player_data[:level],
-                 xp_given: 100
-               ).tap { |p| p.xp = player_data[:xp] }
-             when 'mage'
-               Mage.new(
-                 name: player_data[:name],
-                 hp: player_data[:hp],
-                 attack: player_data[:attack],
-                 defense: player_data[:defense],
-                 level: player_data[:level],
-                 xp_given: 100
-               ).tap { |p| p.xp = player_data[:xp] }
-             else
-               Character.new(name: player_data[:name])
-             end
-
-    # reconstruir inventário
-    inventory = Inventory.new
-    data[:inventory].each do |item_data|
-      klass = Object.const_get(item_data[:type])
-      item = klass.new(item_data[:name])
-      inventory.add(item, item_data[:quantity], item_data[:owner].to_sym)
+    File.open(filename, 'rb') do |file|
+      obj = Marshal.load(file)
+      puts "Object loaded from #{filename}"
+      return obj
     end
+  end
 
-    puts "📖 Save loaded from #{filename}."
+  def load_inventory(filename = "inventory.save")
+    return nil unless File.exist?(filename)
 
-    { player: player, inventory: inventory }
+    File.open(filename, 'rb') do |file|
+      obj = Marshal.load(file)
+      puts "Object loaded from #{filename}"
+      return obj
+    end
   end
 end
